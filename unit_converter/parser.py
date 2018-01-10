dcdfa97de9abdf40384a548d7e70301ea85ef03d
jocelynlopez <jocelyn.lopez.pro@gmail.com>
2018-01-10 13:08:42 +0000

    Refactoring _parse_simple_unit method

--- unit_converter/parser.py
+++ unit_converter/parser.py
@@ -20,45 +20,22 @@ class UnitParser(object):
         return self._parse_simple_unit(unit)**float(power)
 
     @staticmethod
-    def _parse_simple_unit(unit: str) -> Unit:
+    def _parse_simple_unit(unit_s: str) -> Unit:
         """Parse a simple unit.
 
         In other word, parse an unit without a power value.
         """
-        # TODO: Refactoring for an automatic distinction between unit and prefix
-        # Case with a prefix
-        if unit[0] in PREFIXES.keys() and unit[0] not in 'mTd':
-            prefix = unit[0]
-            unit_without_prefix = unit[1:]
+        unit = None
+        for prefix in PREFIXES.keys():
+            if unit_s.startswith(prefix) and unit_s[len(prefix):] in UNITS.keys():
+                prefix = PREFIXES[prefix]
+                unit = UNITS[unit_s[len(prefix):]]
+                exit
+        
+        if unit is None:
+            raise UnitDoesntExistError(unit_s)
 
-        #   da prefix (two letters prefix)
-        elif unit[0] == 'd':
-            if unit[0:2] == 'da':
-                prefix = 'da'
-                unit_without_prefix = unit[2:]
-            else:
-                prefix = unit[0]
-                unit_without_prefix = unit[1:]
-
-        #   m prefix is also an unit : m = meter
-        elif unit[0] == 'm' and len(unit) > 1:
-            prefix = unit[0]
-            unit_without_prefix = unit[1:]
-
-        #   T prefix is also an unit : T = Tesla
-        elif unit[0] == 'T' and len(unit) > 1:
-            prefix = unit[0]
-            unit_without_prefix = unit[1:]
-
-        # Case with no prefix
-        else:
-            prefix = ''
-            unit_without_prefix = unit
-
-        try:
-            return PREFIXES[prefix]*UNITS[unit_without_prefix]
-        except KeyError:
-            raise UnitDoesntExistError(unit_without_prefix)
+        return prefix*unit
 
 
 class QuantityParser(object):
